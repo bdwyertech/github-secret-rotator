25268149c98a0799590c747ae99d4f70ac1bf198
Brian Dwyer <brian.dwyer@broadridge.com>
2021-08-31 12:00:49 -0400

    Fix

--- crypto.go
+++ crypto.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "encoding/base64"
-    "fmt"
-    "golang.org/x/crypto/nacl/box"
+	"encoding/base64"
+	"fmt"
+	"golang.org/x/crypto/nacl/box"
 )
 
 func encryptPlaintext(plaintext, publicKeyB64 string) ([]byte, error) {
-    publicKeyBytes, err := base64.StdEncoding.DecodeString(publicKeyB64)
-    if err != nil {
-        return nil, err
-    }
+	publicKeyBytes, err := base64.StdEncoding.DecodeString(publicKeyB64)
+	if err != nil {
+		return nil, err
+	}
 
-    var publicKeyBytes32 [32]byte
-    copiedLen := copy(publicKeyBytes32[:], publicKeyBytes)
-    if copiedLen == 0 {
-        return nil, fmt.Errorf("could not convert publicKey to bytes")
-    }
+	var publicKeyBytes32 [32]byte
+	copiedLen := copy(publicKeyBytes32[:], publicKeyBytes)
+	if copiedLen == 0 {
+		return nil, fmt.Errorf("could not convert publicKey to bytes")
+	}
 
-    plaintextBytes := []byte(plaintext)
-    var encryptedBytes []byte
+	plaintextBytes := []byte(plaintext)
+	var encryptedBytes []byte
 
-    cipherText, err := box.SealAnonymous(encryptedBytes, plaintextBytes, &publicKeyBytes32, nil)
-    if err != nil {
-        return nil, err
-    }
+	cipherText, err := box.SealAnonymous(encryptedBytes, plaintextBytes, &publicKeyBytes32, nil)
+	if err != nil {
+		return nil, err
+	}
 
-    return cipherText, nil
+	return cipherText, nil
 }
